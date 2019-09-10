ee0f5a3ae2a8e83ed22b42198a40e8fc47cf1d94
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-09-10 23:03:00 +0200

    Re-add Perl lib now with Ninja build

---
+++ implementations/Perl_JSON-Path/main.pl
@@ -0,0 +1,10 @@
+use lib qw(./build/lib/perl5/);
+use JSON::Path;
+use JSON;
+
+my $json = decode_json join("", <STDIN>);
+
+my $jpath   = JSON::Path->new($ARGV[0]);
+my @result   = $jpath->values($json);
+
+print encode_json(\@result) . "\n";
